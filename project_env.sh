d30ce90cc9539fe12b5b69e54e279d4be0c50da6
Daniel Phelps <phelps.db@gmail.com>
2016-02-22 16:37:45 -0700

    First pass copying papa-tools for the maya shelf

--- project_env.sh
+++ project_env.sh
@@ -1,7 +1,8 @@
 export BYU_PROJECT_DIR=/groups/dusk
 export BYU_TOOLS_DIR=/groups/dusk/byu-pipeline-tools
 
+export MAYA_SHELF_DIR=${BYU_TOOLS_DIR}/maya-tools/shelf
+
 export PYTHONPATH=${PYTHONPATH}:${BYU_TOOLS_DIR}
 export PATH=${PATH}:${BYU_TOOLS_DIR}/bin
-
 
